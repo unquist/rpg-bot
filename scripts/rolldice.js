31a4fd52a7b3f1f8a6b1658e5db3f56dc28d0127
unquist <unquist@gmail.com>
2016-06-03 11:19:17 -0400

    advantage

--- scripts/rolldice.js
+++ scripts/rolldice.js
@@ -37,33 +37,47 @@
             var sides = msg.match[4] || 6;
             var bonusType = msg.match[5] || "NAN";
 			      var bonus = msg.match[6] || 0;
+			      var advantage = msg.match[7] || "";
+            
             var rolls = rolldice(sides, num);
             var rollsTotal = 0;
-			if(Number(bonus) > 0)
-			{
-				rollsTotal += Number(bonus);
-			}
-			var result = "rolled " + num + "d" + sides;
-			if(bonusType.indexOf("+") != -1)
-			{
-				result += "+" + bonus;
-			}
-			else if(bonusType.indexOf("-") != -1)
-			{
-				result += "-" + bonus;
-			}
+      			
+      			if(Number(bonus) > 0)
+      			{
+      				rollsTotal += Number(bonus);
+      			}
+      			
+      			var result = "rolled " + num + "d" + sides;
+      			if(bonusType.indexOf("+") != -1)
+      			{
+      				result += "+" + bonus;
+      			}
+      			else if(bonusType.indexOf("-") != -1)
+      			{
+      				result += "-" + bonus;
+      			}
 			
-			result += "\n\nResult: ";
+			      if(advantage.indexOf("dis") != -1)
+			      {
+			        result += "\n\nResult: ";
+			      }
+			      else if(advantage.indexOf("adv") != -1)
+			      {
+			        result += "\n\nResult: ";
+			      }
+			      else
+			      {
+			        result += "\n\nResult: ";
+			      }
+			      
 
             for (var j = 0; j < rolls.length; j++) {
                 result += "`" + rolls[j] + "` ";
-		    }
-
-            if ((rolls.length > 1) || (rolls.length == 1 && Number(bonus) > 0)) {
-                for (var k = 0; k < rolls.length; k++) {
-                    rollsTotal += rolls[k];
-                }
-
+                rollsTotal += rolls[j];
+		        }
+            
+            if ((rolls.length > 1) || (rolls.length == 1 && Number(bonus) > 0)) 
+            {
                 result += "\n\nTotal: `" + rollsTotal + "`";
             }
 	
