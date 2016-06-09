c5ffd7ebdac5f47a035de3dce149ca9595fc0fef
unquist <unquist@gmail.com>
2016-06-09 16:24:44 -0400

    bug fix

--- scripts/rolldice.js
+++ scripts/rolldice.js
@@ -130,7 +130,7 @@
                 "title": "This is a test title",
                 "title_link": "https://www.google.com",
                 "footer": "Dice Rolling Script",
-                "footer_icon": "https://a.fsdn.com/allura/p/kdicegen/icon"
+                "footer_icon": "https://a.fsdn.com/allura/p/kdicegen/icon",
                 "fields": [
                 {
                     "value": "@Foo rolled a *`20`*.",
