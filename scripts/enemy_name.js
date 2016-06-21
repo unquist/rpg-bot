e322ab01aa661f331f4e614a3cb98f91618ddc76
unquist <unquist@gmail.com>
2016-06-21 10:52:32 -0400

    Update enemy_name.js

--- scripts/enemy_name.js
+++ scripts/enemy_name.js
@@ -121,7 +121,8 @@ var getRandomEnemyName = function() {
 	*/
 	//get a number from 1 to 100
 	var probability = Math.round(Math.random() * (99)) + 1;
-	
+	return "mud";
+	/*
 	if(probability <= 30)
 	{
 		var indexA = Math.floor(Math.random()*names.length);
@@ -169,6 +170,7 @@ var getRandomEnemyName = function() {
 		robot.logger.debug("Returning name + epithet ["+name+"]");
 		return name;
 	}
+	*/
 };
 
 var cleanNames = function() {
