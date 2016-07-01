1337d5c2d680f0d6cf82ffb5c7f4dc6430785120
unquist <unquist@gmail.com>
2016-07-01 10:39:26 -0400

    Create inventory.js

---
+++ scripts/inventory.js
@@ -0,0 +1,24 @@
+// Description:
+//   Track party inventory
+//
+// Dependencies:
+//   None
+//
+// Configuration:
+//   None
+//
+// Commands:
+//   
+// Author:
+//   unquist
+
+(function() {
+    module.exports = function(robot) {
+        var util = require("util");
+		var randint = function(sides) {
+            return Math.round(Math.random() * (sides - 1)) + 1;
+        };
+
+
+
+})();
