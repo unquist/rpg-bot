1dc4bda2bfdaa33233d0cfb86daddebc284b94a0
unquist <unquist@gmail.com>
2016-07-01 10:50:58 -0400

    Update inventory.js

--- scripts/inventory.js
+++ scripts/inventory.js
@@ -15,12 +15,12 @@
 (function() {
     module.exports = function(robot) {
       
-      //var GoogleSpreadsheet = require('google-spreadsheet');
+      /*var GoogleSpreadsheet = require('google-spreadsheet');
       
       var util = require("util");
 		  
 		  var test = function() {
             return Math.round(Math.random() * (sides - 1)) + 1;
       };
-
+   */
 })();
