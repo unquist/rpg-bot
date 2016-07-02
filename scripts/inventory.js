cd6d13633a530ff45a8a97b3f22214d623873dd7
Peter Guarnieri <peter.guarnieri@gmail.com>
2016-07-01 22:23:33 -0400

    foo

--- scripts/inventory.js
+++ scripts/inventory.js
@@ -15,12 +15,15 @@
 (function() {
     module.exports = function(robot) {
      
+		const GOOGLE_SERVICE_ACCOUNT = process.env.SERVICE_ACCOUNT;
+		const GOOGLE_PRIVATE_KEY = process.env.GOOGLE_PRIVATE_KEY;
+		
 		var GoogleSpreadsheet = require('google-spreadsheet');
 
         robot.respond(/(inventory)/i, function(msg) {
             var callerName = msg.message.user.name;
 			var command = msg.match[1] || "NAN";
-    
+			GoogleSpreadsheet.useServiceAccountAuth({client_email:GOOGLE_SERVICE_ACCOUNT,private_key:GOOGLE_PRIVATE_KEY}, callback)
             return msg.reply(callerName+" checked their inventory.");
         });
   
