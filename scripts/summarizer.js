75c797a06836b5e9c1c7bcd43506ae1fd2fbba3b
Peter Guarnieri <peter.guarnieri@gmail.com>
2016-07-14 09:22:59 -0400

    foo

--- scripts/summarizer.js
+++ scripts/summarizer.js
@@ -37,6 +37,49 @@
             return Math.round(Math.random() * (sides - 1)) + 1;
         };
 		
+		//filters a message array, and returns the array. Since we assume slack will give us messages in the reverse order, this array re-sorts oldest-to-newest
+		var messageFilter = function(messages)
+		{
+			var filteredMessages = new Array();
+			for(var k = 0; k < messages.length; k++)
+			{
+				var archivedMessage = messages[k];
+				var name = getRealNameFromId(archivedMessage.user);
+				var txt = archivedMessage.text;
+				
+				robot.logger.debug("Msg["+k+"] -> user_id:["+archivedMessage.user+"] user:["+name+"] txt:["+txt+"]");
+				//throw away any system messages
+				if(name == "SYSTEM")
+				{
+					continue;
+				}
+				
+				
+				
+				//regex will find text enclosed by 1 or 3 backticks, or italicized
+				var match = txt.match(/`{1}([^`]+)`{1}|`{3}([^`]+)`{3}|_{1}([^_]+)_{1}/i);
+			 
+				if(match != null)
+				{
+					//found a match, so this message goes into the array.
+					robot.logger.debug("Matched regex; insert into array and continue.");
+					filteredMessages.push(archivedMessage);
+					continue;
+				}
+				
+				//we also want to save "/me" messages
+				if(archivedMessage.subtype == "me_message")
+				{
+					robot.logger.debug("Found /me message. Insert into arrary and continue");
+					filteredMessages.push(archivedMessage);
+					continue;
+				}
+				
+				summaryMessage += "*"+name+"*: " + txt + "\n\n";
+			}
+							
+		}
+		
 		robot.respond(/(summary)\s+(\d+)\s+(hour|hours|minute|minutes|day|days)/i, function(msg) {
             var callerName = msg.message.user.name;
 			//robot.logger.debug(util.inspect(msg));
@@ -49,8 +92,25 @@
 			
 			robot.logger.debug("User ["+callerName+"] asked for the log for ["+numberOfTimeUnits+"] ["+typeOfTimeUnits+"].");
 			
-			targetPastTime.setHours(timeNow.getHours()-numberOfTimeUnits);
+			switch(typeOfTimeUnits)
+			{
+				case "minute":
+				case "minutes":
+					targetPastTime.setMinutes(timeNow.getMinutes()-numberOfTimeUnits);
+					break;
+				case "hour":
+				case "hours":
+					targetPastTime.setHours(timeNow.getHours()-numberOfTimeUnits);
+					break;
+				case "day":
+				case "days":
+					targetPastTime.setDays(timeNow.getDays()-numberOfTimeUnits);
+					break;
+				default:
+					return msg.reply("I didn't recognize ["+typeOfTimeUnits+"]. Valid units are `minutes`, `hours` or `days`.");
+			}
 			
+					
 			//utc:
 			//var utcTime = timeNow.getTime() / 1000;
 			var utcTargetPastTime = (targetPastTime.getTime())/1000;
@@ -66,34 +126,9 @@
 				robot.logger.debug("Successfully retrieved channel history.");
 				//create the message with attachment object
 				var summaryMessage = ""; 
-				for(var k = res.messages.length-1; k >= 0; k--)
-				{
-					var archivedMessage = res.messages[k];
-					var name = getRealNameFromId(archivedMessage.user);
-					//throw away any system messages
-					if(name == "SYSTEM")
-					{
-						continue;
-					}
-					
-					var txt = archivedMessage.text;
-					
-					//throw away any text that starts with quotation formatting
-					if(txt.indexOf("&gt;") != -1)
-					{
-						continue;
-					}
-					
-					if(archivedMessage.subtype == "me_message")
-					{
-						txt = "_"+txt+"_";
-					}
-					
-					summaryMessage += "*"+name+"*: " + txt + "\n\n";
-				}
-							
+				
 				var msgData = {
-					channel: summaryChannelId,
+					channel: "@"+callerName,
 					text: summaryMessage
 				};
 
@@ -108,9 +143,6 @@
         });
 		
 		
-		var getChannelHistory = function (msgObj, robot) {
-
-		}
 		
 /*End function definitions*/
 	};
