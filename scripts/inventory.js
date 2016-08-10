e5d97d529038bc4fc9dc713602d789a7a5c89ffa
unquist <unquist@gmail.com>
2016-08-10 14:49:27 -0400

    google integration

--- scripts/inventory.js
+++ scripts/inventory.js
@@ -15,37 +15,13 @@
 (function() {
     module.exports = function(robot) {
      
-		//const GOOGLE_SERVICE_ACCOUNT = process.env.SERVICE_ACCOUNT;
-		//const GOOGLE_PRIVATE_KEY = process.env.GOOGLE_PRIVATE_KEY;
-		var google = require("googleapis")
+		var google = require("googleapis");
 		var util = require("util");
-		//var GoogleSpreadsheet = require('google-spreadsheet');
-		/*
-        robot.respond(/(inventory)/i, function(msg) {
-            robot.logger.debug("Inventory function");
-			var callerName = msg.message.user.name;
-			var command = msg.match[1] || "NAN";
-			
-			var doc = new GoogleSpreadsheet('1Z9J9onWvwjS8bsXEfdz36jFdFSOHnvJVymFAt_2RUI0');
-			
-			//var creds = require('./google-generated-creds.json');
-			var creds = {
-			private_key: GOOGLE_PRIVATE_KEY,
-			client_email : GOOGLE_SERVICE_ACCOUNT
-			};
-			
-			doc.useServiceAccountAuth(creds, function(message){robot.logger.debug("Message["+message+"]");});
-            robot.logger.debug("returning from inventory function");
-			
-			doc.getInfo(function (info){
-				robot.logger.debug("title=["+info.title+"]");
-			});
-			
-			return msg.reply(callerName+" checked their inventory.");
-        });
-		*/
+		var spreadsheet_wrapper = require('./spreadsheet_wrapper');
+
 		robot.respond(/(inventory)/i, function(msg) {
 			//robot.logger.debug(util.inspect(google));
+			/*
 			var serviceClient = google['sheets']("v4");
 		
 			robot.emit("googleapi:request", {
@@ -68,10 +44,18 @@
 					return msg.reply(result);
 				}
 			});
+			*/
+			//'1Z9J9onWvwjS8bsXEfdz36jFdFSOHnvJVymFAt_2RUI0'
+			//'Party Loot!A11:A17'
+			spreadsheet_wrapper.getSpreadsheetValues('1Z9J9onWvwjS8bsXEfdz36jFdFSOHnvJVymFAt_2RUI0','Party Loot!A11:A17',function(err, data) {
+				if (err) {
+					robot.logger.debug("getSpreadsheetValues error:"+err);
+					return msg.reply(err);
+				}
+				return msg.reply(data.values);
+			});
         });
-
-  
-     
+ 
 };
 
 })();
