f8289b28f99c98e9ecbfdd46cdef8b3a7b46ce0a
Peter Guarnieri <peter.guarnieri@gmail.com>
2016-08-09 21:47:55 -0400

    foo

--- scripts/googleauth.js
+++ scripts/googleauth.js
@@ -23,7 +23,7 @@ ref = process.env, HUBOT_URL = ref.HUBOT_URL, HEROKU_URL = ref.HEROKU_URL, GOOGL
 HUBOT_URL = HUBOT_URL || HEROKU_URL || ("http://" + (require("os").hostname()));
 
 if (HUBOT_URL[HUBOT_URL.length - 1] === "/") {
-  HUBOT_URL = HUBOT_URL.slice(0, -1);
+	HUBOT_URL = HUBOT_URL.slice(0, -1);
 }
 
 AUTH_PATH = "/auth/googleapi";
@@ -35,66 +35,66 @@ SAFETY_MARGIN = 5 * 60 * 1000;
 client = new OAuth2(GOOGLE_API_CLIENT_ID, GOOGLE_API_CLIENT_SECRET, "" + HUBOT_URL + AUTH_PATH + "/callback");
 
 google.options({
-  auth: client
+auth: client
 });
 
 updateCredential = function(robot, callback) {
-  var credential;
-  credential = robot.brain.get(BRAIN_KEY);
-  if (!credential) {
-    return callback(new Error("Needs authorization. Authorize at " + HUBOT_URL + AUTH_PATH));
-  }
-  client.setCredentials(credential);
-  if (Date.now() > credential.expiry_date - SAFETY_MARGIN) {
-    return client.refreshAccessToken(function(err, credential) {
-      if (err) {
-        return callback(err);
-      }
-      robot.brain.set(BRAIN_KEY, credential);
-      return callback(null);
-    });
-  } else {
-    return callback(null);
-  }
+	var credential;
+	credential = robot.brain.get(BRAIN_KEY);
+	if (!credential) {
+		return callback(new Error("Needs authorization. Authorize at " + HUBOT_URL + AUTH_PATH));
+	}
+	client.setCredentials(credential);
+	if (Date.now() > credential.expiry_date - SAFETY_MARGIN) {
+		return client.refreshAccessToken(function(err, credential) {
+			if (err) {
+				return callback(err);
+			}
+			robot.brain.set(BRAIN_KEY, credential);
+			return callback(null);
+		});
+	} else {
+		return callback(null);
+	}
 };
 
 module.exports = function(robot) {
-  robot.respond(/googleapi auth(orize)?$/, function(msg) {
-    return msg.send("Authorize at " + HUBOT_URL + AUTH_PATH);
-  });
-  robot.router.get(AUTH_PATH, function(req, res) {
-    return res.redirect(client.generateAuthUrl({
-      access_type: "offline",
-      approval_prompt: "force",
-      scope: GOOGLE_API_SCOPES.split(",").map(function(e) {
-        return "https://www.googleapis.com/auth/" + (e.trim());
-      })
-    }));
-  });
-  robot.router.get(AUTH_PATH + "/callback", function(req, res) {
-    return client.getToken(req.query.code, function(err, credential) {
-      if (err) {
-        return res.send(err.message);
-      }
-      robot.brain.set(BRAIN_KEY, credential);
-      return res.send("Authorization has succeeded!");
-    });
-  });
-  return robot.on("googleapi:request", function(arg) {
-    var callback, endpoint, params, service, version;
-    service = arg.service, version = arg.version, endpoint = arg.endpoint, params = arg.params, callback = arg.callback;
-    if (version[0] !== "v") {
-      version = "v" + version;
-    }
-    return updateCredential(robot, function(err) {
-      var serviceClient;
-      if (err) {
-        return callback(err);
-      }
-      serviceClient = google[service](version);
-      return endpoint.split(".").reduce((function(a, e) {
-        return a[e];
-      }), serviceClient)(params, callback);
-    });
-  });
+	robot.respond(/googleapi auth(orize)?$/, function(msg) {
+		return msg.send("Authorize at " + HUBOT_URL + AUTH_PATH);
+	});
+	robot.router.get(AUTH_PATH, function(req, res) {
+		return res.redirect(client.generateAuthUrl({
+			access_type: "offline",
+			approval_prompt: "force",
+			scope: GOOGLE_API_SCOPES.split(",").map(function(e) {
+				return "https://www.googleapis.com/auth/" + (e.trim());
+			})
+		}));
+	});
+	robot.router.get(AUTH_PATH + "/callback", function(req, res) {
+		return client.getToken(req.query.code, function(err, credential) {
+			if (err) {
+				return res.send(err.message);
+			}
+			robot.brain.set(BRAIN_KEY, credential);
+			return res.send("Authorization has succeeded!");
+		});
+	});
+	return robot.on("googleapi:request", function(arg) {
+		var callback, endpoint, params, service, version;
+		service = arg.service, version = arg.version, endpoint = arg.endpoint, params = arg.params, callback = arg.callback;
+		if (version[0] !== "v") {
+			version = "v" + version;
+		}
+		return updateCredential(robot, function(err) {
+			var serviceClient;
+			if (err) {
+				return callback(err);
+			}
+			serviceClient = google[service](version);
+			return endpoint.split(".").reduce((function(a, e) {
+				return a[e];
+			}), serviceClient)(params, callback);
+		});
+	});
 };
