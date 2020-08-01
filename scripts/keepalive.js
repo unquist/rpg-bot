cdf89567bc9d8c59a9e66ba4a8223ae29f364519
unquist <unquist@gmail.com>
2020-08-01 17:17:33 -0400

    order command

--- scripts/keepalive.js
+++ scripts/keepalive.js
@@ -16,6 +16,7 @@
 		
 	module.exports = function(robot) {
 		robot.router.post('/hubot/keepalive', function(req, res) {
+			robot.logger.debug("Received a POST request to /hubot/keepalive");
 			robot.brain.set("keepalive","Last keepalive was: " + Date.now());
 		});
 		
