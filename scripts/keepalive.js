d5f5c39ec257dc166ed235b71f9398b3c8bce8e9
unquist <unquist@gmail.com>
2020-08-01 17:34:29 -0400

    keepalive scrint

--- scripts/keepalive.js
+++ scripts/keepalive.js
@@ -17,7 +17,16 @@
 	module.exports = function(robot) {
 		robot.router.post('/hubot/keepalive', function(req, res) {
 			robot.logger.debug("Received a POST request to /hubot/keepalive");
-			robot.brain.set("keepalive","Last keepalive was: " + Date.now());
+			try{
+				robot.brain.set("keepalive","Last keepalive was: " + Date.now());
+				var foo = robot.brain.get("keepalive");
+				return res.status(200).send('Keep alive request succeeded!')
+			}
+			catch (err)
+			{
+				robot.logger.debug("Caught error: " + err.message);
+				return res.status(500).send('Error -> ' + err.message);
+			}
 		});
 		
 	};
